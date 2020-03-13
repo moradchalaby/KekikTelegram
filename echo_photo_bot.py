4fab877044111c0b6550bd00640fa752e6994797
ynsmrkrblt <61253198+ynsmrkrblt@users.noreply.github.com>
2020-03-13 17:13:45 +0300

    Create echo_photo_bot.py

---
+++ echo_photo_bot.py
@@ -0,0 +1,14 @@
+from telebot import TeleBot
+token = ""
+
+bot = TeleBot(token)
+
+@bot.message_handler(content_types=['photo']) #Fotoğraf gönderilirse...
+def fotograf_tekrari(m):    
+     chat_id = m.chat.id #sohbetin ID numarası    
+     file_id = m.photo[-1].file_id #fotoğrafa ait file_id değeri    
+     caption = m.caption #fotoğraf altındaki yazı    
+     bot.send_photo(chat_id, file_id, caption) #Fotoğraf gönder
+
+
+bot.polling() #Gelecek mesajları dinlemeye hazır...
